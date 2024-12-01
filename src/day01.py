023bcb3abf5b363e91d7bf1522b854c44d7d32ce
Fortunoxx <robert_goetz@hotmail.com>
2024-12-01 21:07:32 +0100

    day 01, part 2

--- src/day01.py
+++ src/day01.py
@@ -11,21 +11,32 @@ def get_values(fileInfo, n=1):
             left.append(int(parts[0]))
             right.append(int(parts[1]))
 
-            left.sort()
-            right.sort()
-
         return (left, right)
 
 
 def calc(left, right):
+    # this array will hold the numbers
     deltas = []
+
+    left.sort()
+    right.sort()
+
+    # for each item, find the difference between the number pairs
     for i in range(len(left)):
         deltas.append(abs(left[i] - right[i]))
 
     return sum(deltas)
 
 def calc2(left, right):
-    return 0 # todo
+    # this array will hold the numbers
+    results = []
+
+    # for each value, find the number of times it appears in right
+    for l in left:
+        results.append(l * right.count(l))
+
+    # return the sum of the results
+    return sum(results)
 
 
 def solve_part1(fileInfo):
--- test/test_day01.py
+++ test/test_day01.py
@@ -9,8 +9,7 @@ def test_part1(day, expected_value=11):
     testdata = {"key": "sample", "file": f"test/data/day{day}.sample.dat"}
     assert solve_part1(testdata) == expected_value
 
-# TODO: Implement this function
 @pytest.mark.parametrize("day", [day])
-def test_part2(day, expected_value=0):
+def test_part2(day, expected_value=31):
     testdata = {"key": "sample", "file": f"test/data/day{day}.sample.dat"}
     assert solve_part2(testdata) == expected_value
