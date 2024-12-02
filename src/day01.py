371f04d7fed518f767a3323e854ffab456ca7410
Fortunoxx <robert_goetz@hotmail.com>
2024-12-02 20:41:07 +0100

    day 01 refactoring

--- src/day01.py
+++ src/day01.py
@@ -1,42 +1,16 @@
-# TODO: Implement this function
-def get_values(fileInfo, n=1):
-    left = []
-    right = []
-
-    with open(fileInfo["file"]) as file:
-        for line in file:
-            line = line.replace("\n", "")
-            line = line.replace("   ", ";")
-            parts = line.split(";")
-            left.append(int(parts[0]))
-            right.append(int(parts[1]))
-
-        return (left, right)
+def get_values(fileInfo):
+    file = open(fileInfo["file"]).readlines()
+    left = [int(line.split()[0]) for line in file]
+    right = [int(line.split()[1]) for line in file]
+    return (left, right)
 
 
 def calc(left, right):
-    # this array will hold the numbers
-    deltas = []
+    return sum([abs(sorted(left)[i] - sorted(right)[i]) for i in range(len(left))])
 
-    left.sort()
-    right.sort()
-
-    # for each item, find the difference between the number pairs
-    for i in range(len(left)):
-        deltas.append(abs(left[i] - right[i]))
-
-    return sum(deltas)
 
 def calc2(left, right):
-    # this array will hold the numbers
-    results = []
-
-    # for each value, find the number of times it appears in right
-    for l in left:
-        results.append(l * right.count(l))
-
-    # return the sum of the results
-    return sum(results)
+    return sum([(l * right.count(l)) for l in left])
 
 
 def solve_part1(fileInfo):
